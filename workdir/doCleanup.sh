b78d3e88a0b0fb074003d8fa59ff0748a8d1e39f
Michelangelo Sorice <msorice11@gmail.com>
2018-09-22 19:41:43 +0200

    wip - Chrome profile insertion

---
+++ workdir/doCleanup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+rm -rf results/
+rm -rf ./browsertime-results/
+rm errorLog.txt log.txt
---
+++ workdir/runBrowsertime.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+browser=$1
+url=$2
+domain=`echo $url | cut -d'/' -f3`
+
+dockerOptions=(
+## Amount of memory shared by container process
+'--shm-size=1g'
+## Automatically clean up the container and remove the file system when the container exits
+'--rm'
+## Limiting resource usage RAM 8gb CPUS 2
+'--memory=8000m'
+'--cpus=2'
+)
+
+## More options available at https://www.sitespeed.io/documentation/browsertime/configuration/#the-options
+browsertimeOptions=(
+## Disablig
+'--videoParams.addTimer=false'
+## Recording video with framerate of 10fps
+'--videoParams.framerate=10'
+## Ffmpeg param to modify video quality ranges from 0 (max quality) to 51
+'--videoParams.crf=26'
+## Disabling default filmstrip option as we are going to create scripts in a second moment
+'--videoParams.createFilmstrip=false'
+## Disabling computation of visual metrics
+'--visualMetrics=false'
+## Number of test iterations for each url
+'--iterations=1'
+## Turn this to true to indent json/har files
+'--prettyPrint=false'
+)
+
+docker run "${dockerOptions[@]}" \
+            -e RESULT_DIR=./${browser} \
+            -v "$(pwd)"/results/${domain}:/browsertime mybrowsertime  \
+            "${url}" \
+            "${browsertimeOptions[@]}" \
+            -b ${browser} \
+            --resultDir=./${browser}
+
+
