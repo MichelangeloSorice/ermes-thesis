258e96e3f4e672ccfe916349085b73b9e95f4cbb
Michelangelo Sorice <msorice11@gmail.com>
2018-10-22 21:43:36 +0200

    Big commit with thousands of files

---
+++ resources/browsertime/chrome-4profiles-backup/google-chrome/Profile 4/Extensions/ahnanjpbkghcdgmlchbcfoiefnifjeni/0.2.9_0/data/popup/popup.js
@@ -0,0 +1,174 @@
+var background = (function () {
+  var _tmp = {};
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    for (var id in _tmp) {
+      if (_tmp[id] && (typeof _tmp[id] === "function")) {
+        if (request.path == 'background-to-popup') {
+          if (request.method === id) _tmp[id](request.data);
+        }
+      }
+    }
+  });
+  /*  */
+  return {
+    "receive": function (id, callback) {_tmp[id] = callback},
+    "send": function (id, data) {chrome.runtime.sendMessage({"path": 'popup-to-background', "method": id, "data": data})}
+  }
+})();
+
+var activeTabUrl = '';
+var popupElements = ['bug', 'faq', 'reset', 'media', 'image', 'style', 'object', 'script', 'input-field', 'website-list', 'global-settings', 'input-field-add', 'start-stop-button'];
+var information = "No-Script Suite Lite provides an extra security for your Browser. It allows JavaScript to be executed only by trusted websites of your choice. Few websites are added to the white-list table above by default, but you can edit the list at any time.";
+
+var popupObj = {
+  "state": '',
+  "itemURL": '',
+  "whiteList": [],
+  "mode": ['', '', '', '', ''],
+  "whitelistR": ['', '', '', '', ''],
+  "blacklistR": ['', '', '', '', ''],
+  "address": ["all", "all", "all", "all", "all"],
+  "arr": ["script", "style", "image", "object", "media"],
+  "apply": ["domain", "domain", "domain", "domain", "domain"],
+  "match": ["wildcard", "wildcard", "wildcard", "wildcard", "wildcard"]
+};
+
+var init = function () {
+  var count = 1;
+  var table = document.getElementById('website-list-table');
+  table.textContent = '';
+  var fillTable = function (arr, placeholder) {
+    document.getElementById("input-field").setAttribute("placeholder", placeholder);
+    if (arr && arr.length) {
+      for (var i = arr.length - 1; i >= 0; i--) {
+        var a = document.createElement('a');
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        /*  */
+        td1.textContent = count;
+        td1.setAttribute('type', 'number');
+        td2.appendChild(a);
+        a.href = arr[i];
+        a.textContent = arr[i];
+        a.setAttribute("target", "_blank");
+        a.setAttribute("style", "text-decoration: none; color: #656565");
+        td2.setAttribute('type', 'item');
+        td3.setAttribute('type', 'close');
+        td1.setAttribute("style", "color: #656565");
+        td2.setAttribute("style", "color: #656565");
+        td3.setAttribute("style", "color: #656565");
+        /*  */
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        table.appendChild(tr);
+        count++;
+      }
+    }
+  };
+  /*  */
+  document.getElementById('input-field').value = activeTabUrl;
+  fillTable(popupObj.whiteList, "Add a website address to allow scripts");
+  document.getElementById('input-field').disabled = popupObj.state === "disable";
+  document.getElementById('input-field-add').setAttribute("state", popupObj.state);
+  document.getElementById('start-stop-button').setAttribute("state", popupObj.state);
+  for (var i = 0; i < popupObj.arr.length; i++) document.getElementById(popupObj.arr[i]).setAttribute("mode", popupObj.mode[i]);
+};
+
+var handlePage = function (e) {
+  var id = e.target.getAttribute("id");
+  if (id === "reset") {
+    mscConfirm("Reset", "Do you really want to reset the addon to factory default?\nNote: all stored data will be deleted and your settings will be reset.", function () {
+      background.send("reset-addon");
+    });
+  } else background.send("open-" + id + "-page");
+};
+
+var addInputFieldItem = function (e) {
+  var value = document.getElementById('input-field').value;
+  if (value) {
+    popupObj.whiteList = popupObj.whiteList.filter(function (e) {return e !== value});
+    popupObj.whiteList.push(value);
+    background.send("store-popup-data", popupObj);
+    document.getElementById('input-field').value = '';
+  }
+};
+
+var handleSAClick = function (e) {
+  var selected = false;
+  e.target.setAttribute("mode", e.target.getAttribute("mode") === "selected" ? '' : "selected");
+  for (var i = 0; i < popupObj.arr.length; i++) {
+    if (document.getElementById(popupObj.arr[i]).getAttribute("mode") === "selected") {
+      selected = true;
+      popupObj.mode[i] = "selected";
+    } else popupObj.mode[i] = '';
+  }
+  background.send("store-popup-data", popupObj);
+  if (!selected) background.send("reset-badge");
+};
+
+var updateInformation = function (e) {
+  window.setTimeout(function () {
+    var target = e.target || e.originalTarget;
+    var title = target.getAttribute("title");
+    if (title) document.getElementById('information-span').textContent = title;
+    else document.getElementById('information-span').textContent = information;
+  }, 100);
+};
+
+background.receive('storageData', function (e) {
+  popupObj = e;
+  document.getElementById('input-field').disabled = popupObj.state === "disable";
+  document.getElementById('input-field-add').setAttribute("state", popupObj.state);
+  document.getElementById('start-stop-button').setAttribute("state", popupObj.state);
+  /*  */
+  var a = document.createElement('a');
+  a.href = popupObj.activeTabUrl;
+  document.body.appendChild(a);
+  activeTabUrl = a.protocol + "//" + a.host;
+  document.body.removeChild(a);
+  /*  */
+  init();
+});
+
+var load = function () {
+  background.send('popup-start');
+  window.removeEventListener("load", load, false);
+  /*  */
+  document.getElementById('input-field').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) addInputFieldItem(e);
+  });
+  /*  */
+  document.getElementById('start-stop-button').addEventListener("click", function (e) {
+    var state = e.target.getAttribute("state");
+    state = (state === "enable") ? "disable" : "enable";
+    popupObj.state = state;
+    background.send("store-popup-data", popupObj);
+  });
+  /*  */
+  document.getElementById('website-list-table').addEventListener("click", function (e) {
+    if (e.target.tagName.toLowerCase() === 'td' || e.target.nodeName.toLowerCase() === 'td') {
+      if (e.target.getAttribute('type') === 'close') {
+        var url = e.target.parentNode.childNodes[1].textContent;
+        popupObj.whiteList = popupObj.whiteList.filter(function (e) {return e !== url});
+        background.send("store-popup-data", popupObj);
+      }
+    }
+  });
+  /*  */
+  document.getElementById("faq").addEventListener("click", handlePage);
+  document.getElementById("bug").addEventListener("click", handlePage);
+  document.getElementById("reset").addEventListener("click", handlePage);
+  document.getElementById('information-span').textContent = information;
+  document.getElementById("website-list").setAttribute("title", information);
+  document.getElementById('input-field-add').addEventListener("click", addInputFieldItem);
+  document.getElementById('global-settings').addEventListener("click", function () {background.send("open-options-page")});
+  /*  */
+  for (var i = 0; i < popupObj.arr.length; i++) document.getElementById(popupObj.arr[i]).addEventListener("click", handleSAClick);
+  for (var i = 0; i < popupElements.length; i++) document.getElementById(popupElements[i]).addEventListener("mouseenter", updateInformation);
+};
+
+window.addEventListener("load", load, false);
---
+++ resources/browsertime/chrome-4profiles-backup/google-chrome/Profile 4/Extensions/ahnanjpbkghcdgmlchbcfoiefnifjeni/0.2.9_0/lib/config.js
@@ -0,0 +1,179 @@
+var config = {}, CBN = {};
+
+var configSettingsObject = {
+  "CSP": true,
+  "WRB": false,
+  "HOME": true,
+  "BADGE": true,
+  "itemURL": '',
+  "CONSOLE": false,
+  "INCLUSIVE": true,
+  "state": "enable",
+  "EXCLUSIVE": false,
+  "whitelistR": ['', '', '', '', ''],
+  "blacklistR": ['', '', '', '', ''],
+  "mode": ["selected", '', '', '', ''],
+  "address": ["all", "all", "all", "all", "all"],
+  "arr": ["script", "style", "image", "object", "media"],
+  "apply": ["domain", "domain", "domain", "domain", "domain"],
+  "match": ["wildcard", "wildcard", "wildcard", "wildcard", "wildcard"],
+  "whiteList": [
+    "https://hotmail.com", "https://github.com", "https://live.com",
+    "http://www.ebay.com", "http://www.bing.com", "http://www.apple.com",
+    "https://www.yahoo.com", "http://www.amazon.com", "https://twitter.com",
+    "https://instagram.com", "https://www.chase.com", "https://www.paypal.com",
+    "http://store.apple.com", "https://www.icloud.com", "https://www.google.com",
+    "https://login.live.com", "https://mail.google.com", "https://login.yahoo.com",
+    "https://www.mozilla.org", "https://www.youtube.com",  "https://mail.yahoo.com",
+    "https://www.facebook.com", "https://addons.opera.com", "https://plus.google.com",
+    "https://accounts.google.com", "https://addons.mozilla.org", "http://mybrowseraddon.com",
+    "https://chrome.google.com", "https://translate.google.com", "https://www.bankofamerica.com"
+  ]
+};
+
+var WHITELIST = configSettingsObject.whiteList.join(' | ');
+
+config.welcome = {
+  get open () {return config.settings.object.HOME},
+  get version () {return app.storage.read("version")},
+  set version (val) {app.storage.write("version", val)}
+};
+
+config.init = function () {
+  var _tmp = app.storage.read('noscript-object');
+  if (_tmp) {
+    var obj = JSON.parse(_tmp);
+    configSettingsObject = obj;
+    WHITELIST = configSettingsObject.whiteList.join(' | ');
+  } else {
+    var flag = navigator.userAgent.toLowerCase().indexOf('firefox') !== -1;
+    if (flag) configSettingsObject.HOME = false;
+  }
+};
+
+config.settings = {
+  get whiteList () {return WHITELIST},
+  get object () {return configSettingsObject},
+  set object (o) {
+    configSettingsObject = o;
+    WHITELIST = o.whiteList.join(' | ');
+    app.storage.write('noscript-object', JSON.stringify(o));
+  }
+};
+
+config.hostname = function (url) {
+  var s = url.indexOf("//") + 2;
+  if (s > 1) {
+    var o = url.indexOf('/', s);
+    if (o > 0) return url.substring(s, o);
+    else {
+      o = url.indexOf('?', s);
+      if (o > 0) return url.substring(s, o);
+      else return url.substring(s);
+    }
+  } else return url;
+};
+
+config.badge = {
+  set number (o) {CBN = o},
+  get number () {return CBN},
+  "update": function (top, reset) {
+    var tmp = config.badge.number;
+    tmp[top] = reset ? 0 : (tmp[top] || 0) + 1;
+    config.badge.number = tmp;
+    app.addon.send("update-badge");
+  }
+};
+
+config.address = {
+  "validate": function (top) {
+    var key = config.hostname(top);
+    return config.settings.whiteList.indexOf(key) === -1;
+  },
+  "thirdparty": function (top, url) {
+    var topDomain = config.hostname(top);
+    var currentDomain = config.hostname(url);
+    return currentDomain !== topDomain;
+  }
+};
+
+config.webRequest = function (method, top, url, type) {
+  var CSP = '';
+  if (config.address.validate(top)) {
+    for (var i = 0; i < config.settings.object.mode.length; i++) {
+      if (config.settings.object.mode[i] === "selected") {
+        var flag1 = (config.settings.object.apply[i] === 'frame') ? (type === 'sub_frame') : true;
+        var flag2 = (config.settings.object.address[i] === 'thirdparty') ? config.address.thirdparty(top, url) : true;
+        /*  */
+        var whitelistR = config.settings.object.whitelistR[i];
+        var blacklistR = config.settings.object.blacklistR[i];
+        var whitelisted = whitelistR ? (new RegExp(whitelistR)).test(url) : false;
+        var blacklisted = blacklistR ? (new RegExp(blacklistR)).test(url) : false;
+        /*  */
+        if (config.settings.object.EXCLUSIVE) {
+          if (method === "CSP") {
+            if (config.settings.object.CONSOLE) {
+              console.error("Content Security Policy :: Please use Inclusive mode from the settings page. Note: CSP is an inclusive method. Or, switch to Web Request Observer method, if you need Exclusive mode.");
+            }
+          } else if (blacklisted) {
+            if (flag1 && flag2) {
+              if (method === "WRB") {
+                if (type.indexOf(config.settings.object.arr[i]) !== -1) {
+                  config.badge.update(top, false);
+                  if (config.settings.object.CONSOLE) {
+                    var _rule, _tmp = (new RegExp(blacklistR)).exec(url);
+                    if (_tmp && _tmp.length) _rule = _tmp[0];
+                    console.error("Web Request Observer ::", "Exclusive Mode >>", "BlackList RULE >>", _rule, " TYPE >>", type, " URL >>", url);
+                  }
+                  return true;
+                }
+              }
+            }
+          }
+        } else {
+          if (!whitelisted) {
+            if ((flag1 && flag2) || (blacklisted)) {
+              if (method === "WRB") {
+                if (type.indexOf(config.settings.object.arr[i]) !== -1) {
+                  config.badge.update(top, false);
+                  if (config.settings.object.CONSOLE) {
+                    console.error("Web Request Observer ::", "Inclusive Mode >>", "TYPE >>", type, " URL >>", url);
+                  }
+                  return true;
+                }
+              }
+              if (method === "CSP") {
+                config.badge.update(top, false);
+                var value = ["script-src 'none'", "style-src 'none'", "img-src 'none'", "object-src 'none'", "media-src 'none'"];
+                CSP += value[i] + "; ";
+              }
+            }
+          } else {
+            if (config.settings.object.CONSOLE) {
+              var _rule, _tmp = (new RegExp(whitelistR)).exec(url);
+              if (_tmp && _tmp.length) _rule = _tmp[0];
+              console.error("Web Request Observer ::", "Inclusive Mode >>", "WhiteList RULE >>", _rule, " TYPE >>", type, " URL >>", url);
+            }
+          }
+        }
+      }
+    }
+  }
+  /*  */
+  if (method === "WRB") return false;
+  if (method === "CSP") {
+    if (config.settings.object.CONSOLE && CSP) console.error("Content Security Policy ::", "Inclusive Mode >>", "POLICY >>", CSP);
+    return CSP;
+  }
+};
+
+config.get = function (name) {return name.split('.').reduce(function (p, c) {return p[c]}, config)};
+
+config.set = function (name, value) {
+  function set(name, value, scope) {
+    name = name.split('.');
+    if (name.length > 1) set.call((scope || this)[name.shift()], name.join('.'), value);
+    else this[name[0]] = value;
+  }
+  set(name, value, config);
+};
