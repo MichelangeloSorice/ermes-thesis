e6e8d00b7c0bab1c3e3797865d9c84b8f283db4f
Michelangelo Sorice <msorice11@gmail.com>
2018-11-05 20:19:59 +0100

    implemented plotting of cdf for base metrics

--- dynamicSectionsDetectionTool/resultsEvaluator.py
+++ dynamicSectionsDetectionTool/resultsEvaluator.py
@@ -1,5 +1,8 @@
 import json
+import shutil
 import sys
+from os import makedirs
+from os.path import exists
 
 import numpy as np
 from matplotlib import pyplot
@@ -49,11 +52,11 @@ def evaluateResults(results, configurationData, nn0BinStep, pdeBinStep):
     # to compute the declared indicators
     for index, blockResult in enumerate(perBlockResult):
         isBlockDynamic = dynamicBlockSummary[index]
-        overallNonZeroCounts.append(blockResult['nonZeroCounts'])
+        overallNonZeroCounts.extend(blockResult['nonZeroCounts'])
         if isBlockDynamic:
-            nonZeroCountsForDynamicBlocks.append(blockResult['nonZeroCounts'])
+            nonZeroCountsForDynamicBlocks.extend(blockResult['nonZeroCounts'])
         else:
-            nonZeroCountsForStaticBlocks.append(blockResult['nonZeroCounts'])
+            nonZeroCountsForStaticBlocks.extend(blockResult['nonZeroCounts'])
 
         for countNonZero in blockResult['nonZeroCounts']:
             nn0BinIndex = countNonZero // nn0BinStep
@@ -96,9 +99,10 @@ def evaluateResults(results, configurationData, nn0BinStep, pdeBinStep):
 
 def plotData(outFolder, data):
     nn0Bins, pdeBins = data['nn0'], data['pde']
-    overallNn0Values, overallPdeValues = data['nn0Counts']['overall'], data['pdeValues']['overall']
-    staticBlocksNn0Values, staticBlocksPdeValues = data['nn0Counts']['static'], data['pdeValues']['static']
-    dynamicBlocksNn0Values, dynamicBlocksPdeValues = data['nn0Counts']['dynamic'], data['pdeValues']['dynamic']
+    overallNn0Values, overallPdeValues = np.array([data['nn0Counts']['overall'], data['pdeValues']['overall']])
+    staticBlocksNn0Values, staticBlocksPdeValues = np.array([data['nn0Counts']['static'], data['pdeValues']['static']])
+    dynamicBlocksNn0Values, dynamicBlocksPdeValues = np.array(
+        [data['nn0Counts']['dynamic'], data['pdeValues']['dynamic']])
 
     # Plotting probability or a block to be dynamic given a certain value of NN0
     # nn0X = range(0, len(nn0Bins))
@@ -130,10 +134,58 @@ def plotData(outFolder, data):
     # pyplot.savefig(outFolder + 'dynamicProbForPdeValues.png')
 
     print('Here we go')
-    print(np.array(staticBlocksNn0Values))
-    # Plotting histograms of dynamic and static blocks distributions among NN0 values bins
-    pyplot.hist(np.array(dynamicBlocksNn0Values), bins=100, density=True)
-    pyplot.savefig(outFolder + 'pdfStaticNN0.png')
+
+    # Plotting histograms of binned NN0 values distributions for dynamic and static blocks
+    n, bins, patches = pyplot.hist(dynamicBlocksNn0Values, density=True, cumulative=True, histtype='step')
+    pyplot.savefig(outFolder + 'cdf_NN0_dynamicBlocks.png')
+    pyplot.clf()
+
+    pyplot.hist(staticBlocksNn0Values, density=True, cumulative=True, histtype='step')
+    pyplot.savefig(outFolder + 'cdf_NN0_staticBlocks.png')
+    pyplot.clf()
+
+    pyplot.hist(overallNn0Values, density=True, cumulative=True, histtype='step')
+    pyplot.savefig(outFolder + 'cdf_NN0_allBlocks.png')
+    pyplot.clf()
+
+    pyplot.hist(np.array([overallNn0Values, staticBlocksNn0Values, dynamicBlocksNn0Values]), bins=50,
+                color=['red', 'blue', 'green'], )
+    pyplot.legend(['All blocks', 'Static', 'Dynamic'], loc=1)
+    pyplot.savefig(outFolder + 'hist_NN0_summaryNotNormalized.png')
+    pyplot.clf()
+
+    # Plotting histograms of PDE values distributions for dynamic and static blocks
+    pyplot.hist(dynamicBlocksPdeValues, density=True, cumulative=True, histtype='step')
+    pyplot.savefig(outFolder + 'hist_PDE_dynamicBlocks.png')
+    pyplot.clf()
+
+    pyplot.hist(staticBlocksPdeValues, density=True, cumulative=True, histtype='step')
+    pyplot.savefig(outFolder + 'hist_PDE_staticBlocks.png')
+    pyplot.clf()
+
+    pyplot.hist(overallPdeValues, density=True, cumulative=True, histtype='step')
+    pyplot.savefig(outFolder + 'hist_PDE_allBlocks.png')
+    pyplot.clf()
+
+    pyplot.hist(np.array([overallPdeValues, staticBlocksPdeValues, dynamicBlocksPdeValues]),
+                color=['red', 'blue', 'green'], )
+    pyplot.legend(['All blocks', 'Static', 'Dynamic'], loc=1)
+    pyplot.savefig(outFolder + 'hist_PDE_summaryNotNormalized.png')
+    pyplot.clf()
+
+    # Plotting probability density function
+    # # test values for the bw_method option ('None' is the default value)
+    # bw_values = [None,  0.1]
+    #
+    # # generate a list of kde estimators for each bw
+    # kde = [scipyStats.kde.gaussian_kde(dynamicBlocksNn0Values, bw_method=bw) for bw in bw_values]
+    #
+    # # plot density estimates
+    # t_range = np.linspace(0, 300, 200)
+    # for i, bw in enumerate(bw_values):
+    #     pyplot.plot(t_range, kde[i].evaluate(t_range), label='bw = 0.2')
+    #
+    # pyplot.clf()
 
     # fastplot.plot((nn0X, nn0Y), outFolder + 'nn0Plot.png', xlabel='bins', ylabel='DynamicBlockProbability')
     # fastplot.plot((pdeX, pdeY), outFolder + 'pdePlot.png', xlabel='bins', ylabel='DynamicBlockProbability')
@@ -151,7 +203,14 @@ def main():
         pageConfiguration = json.load(inputFile)
 
     distributionData = evaluateResults(perBlockResults, pageConfiguration, 3, 1)
-    plotData(workdir + '/output/plot', distributionData)
+
+    plotOutFolder = workdir + '/output/plots/'
+    if not exists(plotOutFolder):
+        makedirs(plotOutFolder)
+    else:
+        shutil.rmtree(plotOutFolder)
+        makedirs(plotOutFolder)
+    plotData(plotOutFolder, distributionData)
 
     with open(workdir + '/output/probabilityDistributionData.json', 'a+') as outfile:
         json.dump(distributionData, outfile, indent=2)
